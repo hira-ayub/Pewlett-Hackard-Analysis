284eccc9cba9d397d576e8e52663d96cf94cc19a
Hira Ayub <hira_ayub84@yahoo.com>
2021-04-01 21:34:15 -0400

    adding final code

--- Employee_Database_challenge.sql
+++ Employee_Database_challenge.sql
@@ -15,7 +15,16 @@ INTO unique_titles
 from employees as e
 inner join titles as t
 ON e.emp_no = t.emp_no
-where (birth_date between '1952-01-01' AND '1955-12-31')
+where (e.birth_date between '1952-01-01' AND '1955-12-31')
 ORDER BY e.emp_no, t.to_date DESC;
 
 select * from unique_titles
+limit 10;
+
+select count(emp_no) as "count", title
+into retiring_titles
+from unique_titles
+group by title
+order by "count" desc;
+
+select * from retiring_titles
